edc355bae58fd6db5af02d560809e357cff4029b
yadul <yadulmanoj22@gmail.com>
2024-06-22 15:28:12 +0530

    Added logic for post and put requests

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 const app = express();
 
 app.use(express.json());
 
-app.post("/todo", function (req, res) {});
+app.post("/todo", function (req, res) {
+    const createPayLoad = req.body;
+    const parsedPayload = createTodo.safeParse(createPayLoad);
+
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            message: "Invalid inputs.",
+        });
+        return;
+    }
+});
 
 app.get("/todos", function (req, res) {});
 
-app.put("/completed", function (req, res) {});
+app.put("/completed", function (req, res) {
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            message: "Invalid inputs.",
+        });
+        return;
+    }
+});
